ab311a287b43b27feb139c364b7b2166b4bc06b4
noone <nobody@nowhere.com>
2023-05-18 15:26:30 +0800

    update

--- extensions/copy-tee-bin.sh
+++ extensions/copy-tee-bin.sh
@@ -1,3 +1,3 @@
 function pre_config_uboot_target__copy-tee-bin() {
-    run_host_command_logged cp $USERPATCHES_PATH/overlay/tee.bin $SRC/cache/sources/$BOOTSOURCEDIR
+    run_host_command_logged cp -pv $USERPATCHES_PATH/overlay/tee.bin $SRC/cache/sources/$BOOTSOURCEDIR
 }
