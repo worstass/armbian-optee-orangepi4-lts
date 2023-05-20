76fd80779b94ad455677e98e994e4e9e81dc579c
javagg <443312+javagg@users.noreply.github.com>
2023-05-20 12:36:03 +0800

    Update enable_optee_in_kernel.sh

--- extensions/enable_optee_in_kernel.sh
+++ extensions/enable_optee_in_kernel.sh
@@ -1,4 +1,4 @@
-function armbian_kernel_config__enable_optee_in_kernel() {
+function custom_kernel_config__enable_optee_in_kernel() {
     if [[ -f .config ]]; then
         display_alert "Enabling tee and optee" "armbian-kernel" "debug"
         kernel_config_set_y CONFIG_TEE
