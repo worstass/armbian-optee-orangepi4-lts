2166a98a714a9c39c22723b70277d45732e14de2
nobody <nobody@nowhere.com>
2023-05-31 23:56:41 +0800

    fix

--- extensions/build_uboot_with_optee.sh
+++ extensions/build_uboot_with_optee.sh
@@ -2,26 +2,12 @@ function build_custom_uboot__with_optee() {
     local uboot_work_dir=""
     uboot_work_dir="$(pwd)"
 
-##    ATFBRANCH="tag:v2.7"
-##    target_make="bl31"
-##    display_alert "Downloading sources" "atf" "git"
-##    fetch_from_repo "$ATFSOURCE" "$ATFDIR" "$ATFBRANCH" "yes"
-##    local atf_src_dir=${SRC}/cache/sources/${ATFDIR}/$(branch2dir ${ATFBRANCH})
-##
-##    cd $atf_src_dir || exit
-##
-##    display_alert "Compiling ATF" "" "info"
-##
-##    ATF_COMPILER_64="aarch64-linux-gnu-"
-##    ATF_COMPILER_32="arm-linux-gnueabihf-"
-##
-##    run_host_command_logged CCACHE_BASEDIR="$(pwd)" PATH="${toolchain}:${toolchain2}:${PATH}" \
-##        "CFLAGS='-fdiagnostics-color=always -Wno-error=attributes -Wno-error=incompatible-pointer-types'" \
-##        CROSS_COMPILE="'${CCACHE} ${ATF_COMPILER_64}'" M0_CROSS_COMPILE="$ATF_COMPILER_32" \
-##        make ENABLE_BACKTRACE=0 LOG_LEVEL=40 BUILD_STRING=armbian ARCH=aarch64 PLAT=rk3399 SPD=opteed DEBUG=0 $target_make ${CTHREADS}
+    declare -g ATFBRANCH="tag:v2.7"
+    declare -g ATFSOURCEDIR=${ATFDIR}/$(branch2dir ${ATFBRANCH})
+    compile_atf
 
     OPTEEOSSOURCE="https://github.com/OP-TEE/optee_os.git"
-    OPTEEOSBRANCH="tag:3.20.0"
+    OPTEEOSBRANCH="tag:3.21.0"
     OPTEEOSDIR="optee_os"
     OPTEEOS_COMPILER_64='aarch64-linux-gnu-'
     OPTEEOS_COMPILER_32='arm-linux-gnueabihf-'
@@ -42,6 +28,6 @@ function build_custom_uboot__with_optee() {
     #run_host_command_logged cp -pv "${opteeosdir}/out/arm/core"/*.bin "${opteeosdir}/out/arm/core"/*.elf ./
 
     #UBOOT_TARGET_MAP="BL31=bl31.elf idbloader.img u-boot.itb;;idbloader.img u-boot.itb"
-    UBOOT_TARGET_MAP="BL31=bl31.elf TEE=tee.elf all;;idbloader.img u-boot.itb"
+    UBOOT_TARGET_MAP="BINMAN_VERBOSE=3 BL31=bl31.elf TEE=tee.elf all;;idbloader.img u-boot.itb"
     loop_over_uboot_targets_and_do compile_uboot_target
 }
